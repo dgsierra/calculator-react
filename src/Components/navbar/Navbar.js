107c001269b2a53dceca2777f60cfedf52e762cc
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-04 22:39:15 -0500

    Remove default code and init componet Navbar

---
+++ src/Components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <h2 className="navbar-brand">Math Magicians</h2>
+    <ul className="navbar-elements">
+      <li className="navbar-element">Home</li>
+      <li className="navbar-element">Calculator</li>
+      <li className="navbar-element">Quote</li>
+    </ul>
+  </nav>);
+}
+
+export default Navbar;
