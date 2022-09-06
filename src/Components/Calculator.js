16f4d91ac16b3eb3ee818886dc23e616eec2b5e0
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-06 15:29:06 -0500

    Fix Stylelints

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
 
 export default class Calculator extends Component {
   render() {
     return (
-      <div className='calculatordiv'>
-        <input type="number" className="calcresult"></input>
+      <div className="calculatordiv">
+        <input type="number" className="calcresult" />
         <div className="row1 calculatorrow">
-            <button className="graybtn">AC</button>
-            <button className="graybtn">+/-</button>
-            <button className="graybtn">%</button>
-            <button className="orangebtn">÷</button>
+          <button type="submit" className="graybtn">AC</button>
+          <button type="submit" className="graybtn">+/-</button>
+          <button type="submit" className="graybtn">%</button>
+          <button type="submit" className="orangebtn">÷</button>
         </div>
         <div className="row2 calculatorrow">
-            <button className="graybtn">7</button>
-            <button className="graybtn">8</button>
-            <button className="graybtn">9</button>
-            <button className="orangebtn">X</button>
+          <button type="submit" className="graybtn">7</button>
+          <button type="submit" className="graybtn">8</button>
+          <button type="submit" className="graybtn">9</button>
+          <button type="submit" className="orangebtn">X</button>
         </div>
         <div className="row3 calculatorrow">
-            <button className="graybtn">4</button>
-            <button className="graybtn">5</button>
-            <button className="graybtn">6</button>
-            <button className="orangebtn">-</button>
+          <button type="submit" className="graybtn">4</button>
+          <button type="submit" className="graybtn">5</button>
+          <button type="submit" className="graybtn">6</button>
+          <button type="submit" className="orangebtn">-</button>
         </div>
         <div className="row4 calculatorrow">
-            <button className="graybtn">1</button>
-            <button className="graybtn">2</button>
-            <button className="graybtn">3</button>
-            <button className="orangebtn">+</button>
+          <button type="submit" className="graybtn">1</button>
+          <button type="submit" className="graybtn">2</button>
+          <button type="submit" className="graybtn">3</button>
+          <button type="submit" className="orangebtn">+</button>
         </div>
         <div className="row5 calculatorrow">
-            <button id="cero" className="graybtn">0</button>
-            <button className="graybtn">.</button>
-            <button className="orangebtn">=</button>
+          <button type="submit" id="cero" className="graybtn">0</button>
+          <button type="submit" className="graybtn">.</button>
+          <button type="submit" className="orangebtn">=</button>
         </div>
       </div>
-    )
+    );
   }
 }
