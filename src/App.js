fb90a994b75178e91d2b9f2345def0d47024a133
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-06 10:21:39 -0500

    Add linters

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './Components/Calculator';
+import Navbar from './Components/Navbar.js';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+      <Navbar />
+      <Calculator />
     </div>
-  );
+    )
 }
 
 export default App;
