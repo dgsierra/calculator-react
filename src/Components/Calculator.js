a60c8da0ee3cd3f85e589902941ad4b5f78db220
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-06 01:04:11 -0500

    Add style to calculator component

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -3,34 +3,34 @@ import React, { Component } from 'react'
 export default class Calculator extends Component {
   render() {
     return (
-      <div>
+      <div className='calculatordiv'>
         <input type="number" className="calcresult"></input>
-        <div className="row1">
+        <div className="row1 calculatorrow">
             <button className="graybtn">AC</button>
             <button className="graybtn">+/-</button>
             <button className="graybtn">%</button>
             <button className="orangebtn">÷</button>
         </div>
-        <div className="row2">
+        <div className="row2 calculatorrow">
             <button className="graybtn">7</button>
             <button className="graybtn">8</button>
             <button className="graybtn">9</button>
             <button className="orangebtn">X</button>
         </div>
-        <div className="row3">
+        <div className="row3 calculatorrow">
             <button className="graybtn">4</button>
             <button className="graybtn">5</button>
             <button className="graybtn">6</button>
             <button className="orangebtn">-</button>
         </div>
-        <div className="row4">
+        <div className="row4 calculatorrow">
             <button className="graybtn">1</button>
             <button className="graybtn">2</button>
             <button className="graybtn">3</button>
             <button className="orangebtn">+</button>
         </div>
-        <div className="row5">
-            <button className="graybtn">0</button>
+        <div className="row5 calculatorrow">
+            <button id="cero" className="graybtn">0</button>
             <button className="graybtn">.</button>
             <button className="orangebtn">=</button>
         </div>
