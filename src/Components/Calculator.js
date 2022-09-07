f144a5e6a91ca715435c5b35ee67428b4c09633a
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-07 14:09:29 -0500

    Install big, add onClick and functions

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,39 +1,67 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
+import operate from '../logic/operate';
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = (e) => {
+      e.preventDefault();
+      console.log(e.target.value);
+      // this.setState((state) => calculate(state, e.target.name));
+    }
+  }
+
   render() {
+    const { total, next, operation } = this.state;
+    console.log(total, next, operation);
+    console.log(this.state);
+    // console.log(this.handleClick)
+    console.log(operate(5, 5, 'x'));
+    // console.log(calculate({ total: 100, next: 10, operation: 'x' }, 'x'));
     return (
       <div className="calculatordiv">
-        <input type="number" className="calcresult" />
+        <div id="operation">
+          <div className="calcresult">
+            {total}
+            {next}
+          </div>
+          <div className="symbol">{operation}</div>
+        </div>
         <div className="row1 calculatorrow">
-          <button type="submit" className="graybtn">AC</button>
-          <button type="submit" className="graybtn">+/-</button>
-          <button type="submit" className="graybtn">%</button>
-          <button type="submit" className="orangebtn">÷</button>
+          <button value="AC" onClick={this.handleClick} type="submit" className="graybtn">AC</button>
+          <button value="+/-" onClick={this.handleClick} type="submit" className="graybtn">+/-</button>
+          <button value="%" onClick={this.handleClick} type="submit" className="graybtn">%</button>
+          <button value="÷" onClick={this.handleClick} type="submit" className="orangebtn">÷</button>
         </div>
         <div className="row2 calculatorrow">
-          <button type="submit" className="graybtn">7</button>
-          <button type="submit" className="graybtn">8</button>
-          <button type="submit" className="graybtn">9</button>
-          <button type="submit" className="orangebtn">X</button>
+          <button value={7} onClick={this.handleClick} type="submit" className="graybtn">7</button>
+          <button value={8} onClick={this.handleClick} type="submit" className="graybtn">8</button>
+          <button value={9} onClick={this.handleClick} type="submit" className="graybtn">9</button>
+          <button value="x" onClick={this.handleClick} type="submit" className="orangebtn">X</button>
         </div>
         <div className="row3 calculatorrow">
-          <button type="submit" className="graybtn">4</button>
-          <button type="submit" className="graybtn">5</button>
-          <button type="submit" className="graybtn">6</button>
-          <button type="submit" className="orangebtn">-</button>
+          <button value={4} onClick={this.handleClick} type="submit" className="graybtn">4</button>
+          <button value={5} onClick={this.handleClick} type="submit" className="graybtn">5</button>
+          <button value={6} onClick={this.handleClick} type="submit" className="graybtn">6</button>
+          <button value="-" onClick={this.handleClick} type="submit" className="orangebtn">-</button>
         </div>
         <div className="row4 calculatorrow">
-          <button type="submit" className="graybtn">1</button>
-          <button type="submit" className="graybtn">2</button>
-          <button type="submit" className="graybtn">3</button>
-          <button type="submit" className="orangebtn">+</button>
+          <button value={1} onClick={this.handleClick} type="submit" className="graybtn">1</button>
+          <button value={2} onClick={this.handleClick} type="submit" className="graybtn">2</button>
+          <button value={3} onClick={this.handleClick} type="submit" className="graybtn">3</button>
+          <button value="+" onClick={this.handleClick} type="submit" className="orangebtn">+</button>
         </div>
         <div className="row5 calculatorrow">
-          <button type="submit" id="cero" className="graybtn">0</button>
-          <button type="submit" className="graybtn">.</button>
-          <button type="submit" className="orangebtn">=</button>
+          <button value={0} onClick={this.handleClick} type="submit" id="cero" className="graybtn">0</button>
+          <button value="." onClick={this.handleClick} type="submit" className="graybtn">.</button>
+          <button value="=" onClick={this.handleClick} type="submit" className="orangebtn">=</button>
         </div>
       </div>
     );
