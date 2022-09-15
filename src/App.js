23e992b2a6d76f6182f6c50357d0088b019bcc75
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-09-14 23:33:57 -0500

    Add API call and style to Quote and Home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 /* import Calculator from './Components/Calculator'; */
 import Navbar from './Components/Navbar';
-import Calculator2 from './Components/Calculator2';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Calculator2 />
     </div>
   );
 }
