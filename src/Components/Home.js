067530ddd067cb792e8fa7ce205a4c520274aa22
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-10-04 15:21:00 -0500

    Update welcome menssafe and fix README.md

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,22 +6,9 @@ export default function Home() {
     <div className="home-container">
       <h1 className="text">Welcome to my calculator!</h1>
       <p className="home-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas gravida lacinia rhoncus.
-        Curabitur convallis urna eget nulla pulvinar ultricies. Aenean dolor nunc,
-        hendrerit id odio eu,
-        suscipit accumsan nisi. Proin rhoncus felis dignissim metus lacinia, eget vehicula felis
-        sagittis.
-        Donec eget tortor lectus. In ex diam, tristique et magna vitae, dapibus porta erat.
-        Integer ultricies nisl eget mi semper congue. In hac habitasse platea dictumst.
-        Mauris placerat erat sem, sed accumsan sem euismod et. Mauris in pulvinar eros,
-        vel malesuada odio. Praesent viverra orci et dolor molestie, vitae imperdiet odio posuere.
-        Ut ultrices ligula non gravida tincidunt. Pellentesque at egestas ante.
-
-        Nam lacus ante, pretium ut sem nec, aliquam aliquam nisl. Suspendisse molestie
-        eros eu massa pulvinar, non vehicula massa facilisis. Sed et posuere lectus,
-        vitae gravida diam. Nullam a turpis vel lacus dictum congue. Integer at tempus
-        sapien, non posuere massa. Sed a lorem at ante interdum dignissim. Phasellus
-        ornare id erat vel consequat.
+        This is a small project that give you the ability to do simple calculations.
+        <br />
+        You also can get a random Quote in the Quote tab.
       </p>
     </div>
   );
